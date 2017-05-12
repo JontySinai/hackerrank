28a0275284170a10dedbafba1ec29e85e5ca82a2
Jonty Sinai <jontysinai@gmail.com>
2017-05-12 14:01:08 +0200

    Day 7 solved

--- 10_Days_Stats/day7_spearman_rank_co.py
+++ 10_Days_Stats/day7_spearman_rank_co.py
@@ -4,6 +4,8 @@ import sys
 
 from math import sqrt
 from operator import mul as multiply
+from operator import sub as subtract
+from operator import pow as exponent
 
 # inp = sys.stdin.readlines()
 
@@ -12,9 +14,6 @@ inp = ['10\r\n', '10 9.8 8 7.8 7.7 1.7 6 5 1.4 2 \r\n', '200 44 32 24 22 17 15 1
 n = int(inp[0].replace('\r\n',''))
 X = map(float, inp[1].replace(' \r\n','').split(' '))
 Y = map(float, inp[2].split(' '))
-
-temp_X = map(float, inp[1].replace(' \r\n','').split(' '))
-temp_Y = map(float, inp[2].split(' '))
 
 def get_mean(data, n=-1):
     """
@@ -41,13 +40,14 @@ def get_std_dev(data, n = -1):
 
 
 def get_rank(data, n):
+    temp = data
     rank = range(0,n)
-    max_hack = max(data) + 1
+    max_hack = max(temp) + 1
     counter = 1
     for i in range(0,n):
-        next_min = min(data)
-        pos = data.index(next_min)
-        data[pos] = max_hack
+        next_min = min(temp)
+        pos = temp.index(next_min)
+        temp[pos] = max_hack
         rank[pos] = counter
         counter += 1
     return rank
@@ -83,9 +83,22 @@ def spearman_rank_co(X,Y,n):
     rank_X = get_rank(X, n)
     rank_Y = get_rank(Y, n)
 
-    print rank_X
-    print rank_Y
-
     return pearson_cor_co(rank_X, rank_Y, n)
 
-print round(spearman_rank_co(temp_X, temp_Y, n), 3)
+def spearman_rank_unique(X,Y,n):
+    """
+    Returns the Spearman Rank Coefficient of two random variables, X and Y, with the
+    same number of observations, n, and unique values in each set of observations. This 
+    method uses a special case of the rank coefficient.
+    """
+
+    rank_X = get_rank(X, n)
+    rank_Y = get_rank(Y, n)
+
+    twos = [2]*n
+    diff = map(exponent, map(subtract, rank_X, rank_Y), twos)
+
+    return 1 - 6*float(sum(diff))/(n*(n**2 - 1))
+
+# print round(spearman_rank_co(X, Y, n), 3)
+print round(spearman_rank_unique(X, Y, n), 3)
